7b6159a5d69532397a2cd31243a6e686c0c8617d
Ankit <ankitupa7799@gmail.com>
2021-11-06 15:36:20 +0530

    Updates

---
+++ precache-manifest.7f5c62d967d60a7ab9f108b09175aeb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe871560209341304048a82d670e5d1",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "13bc3a1bb0d498d55e27",
+    "url": "/todolist/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "3be5df589303b1d705aa",
+    "url": "/todolist/static/css/main.bbd96fac.chunk.css"
+  },
+  {
+    "revision": "13bc3a1bb0d498d55e27",
+    "url": "/todolist/static/js/2.f75ab7b4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist/static/js/2.f75ab7b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be5df589303b1d705aa",
+    "url": "/todolist/static/js/main.c824469f.chunk.js"
+  },
+  {
+    "revision": "7558fb00b4542e238607",
+    "url": "/todolist/static/js/runtime-main.97699757.js"
+  }
+]);
